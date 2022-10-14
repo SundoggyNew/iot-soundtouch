1927c791b7f3c33c2bafc37c3a8733eaffce1664
whalensun(孙伟) <532125505@qq.com>
2022-10-14 10:24:55 +0800

    First commit create iot soundtouch aar project

---
+++ android_soundtouch/src/main/java/com/iot/soundtouch/interfaces/SoundTouch.java
@@ -0,0 +1,65 @@
+////////////////////////////////////////////////////////////////////////////////
+///
+/// Example class that invokes native SoundTouch routines through the JNI
+/// interface.
+///
+/// Author        : Copyright (c) Olli Parviainen
+/// Author e-mail : oparviai 'at' iki.fi
+/// WWW           : http://www.surina.net
+///
+////////////////////////////////////////////////////////////////////////////////
+
+package com.iot.soundtouch.interfaces;
+
+public class SoundTouch {
+
+    static {
+        System.loadLibrary("iot-soundtouch");
+    }
+
+    private int channels, samplingRate, bytesPerSample;
+    private float tempo;
+    private int pitchSemi;
+    private int track;
+
+    private static final int BUFFER_SIZE_PUT = 4096;
+
+    public SoundTouch(int track, int channels, int samplingRate, int bytesPerSample, float tempo, int pitchSemi) {
+
+        this.channels = channels;
+        this.samplingRate = samplingRate;
+        this.bytesPerSample = bytesPerSample;
+        this.tempo = tempo;
+        this.pitchSemi = pitchSemi;
+        this.track = track;
+
+        setup(track, channels, samplingRate, bytesPerSample, tempo, pitchSemi);
+    }
+
+    private static synchronized native final void setup(int track, int channels, int samplingRate, int bytesPerSample, float tempo, int pitchSemi);
+
+    private static synchronized native final void putBytes(int track, byte[] input, int length);
+
+    private static synchronized native final int getBytes(int track, byte[] output, int toGet);
+
+    private static synchronized native final void finish(int track, int bufSize);
+
+    private static synchronized native final void clearBytes(int track);
+
+    public void clearBuffer(int track) {
+        clearBytes(track);
+    }
+
+    public void putBytes(byte[] input) {
+        putBytes(track, input, input.length);
+    }
+
+    public int getBytes(byte[] output) {
+        return getBytes(track, output, output.length);
+    }
+
+    //call finish after the last bytes have been written
+    public void finish() {
+        finish(track, BUFFER_SIZE_PUT);
+    }
+}
